24f1b7800393636fba31facd076fe7b851c688e6
yashchaturvedi18 <yash.chaturvedi18@gmail.com>
2023-06-28 13:20:00 -0400

    moving to repo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -29,7 +29,7 @@ const Sidebar = () => {
         to="/"
         onClick={() => setShowNav(false)}>
         <img src={LogoS} alt="Logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        <img className="sub-logo" src={LogoSubtitle} alt="yash" />
       </Link>
       <nav className={showNav ? 'mobile-show' : ''}>
         <NavLink 
